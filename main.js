7b7629551fd96beb4b121338c6ce6c774ff42faf
Lucas Oliveira <lucasnoliveira126@gmail.com>
2023-09-11 20:14:21 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let moveCounter = 0;
+let isMoving = false;
+
+function moveButton() {
+    const nopeButton = document.querySelector('.nope');
+    const randomX = Math.floor(Math.random() * (window.innerWidth - nopeButton.clientWidth));
+    const randomY = Math.floor(Math.random() * (window.innerHeight - nopeButton.clientHeight));
+    nopeButton.style.left = `${randomX}px`;
+    nopeButton.style.top = `${randomY}px`;
+    
+    if (isMoving) {
+        requestAnimationFrame(moveButton);
+    }
+}
+
+function toggleMove() {
+    if (moveCounter % 2 === 0) {
+        isMoving = true;
+        moveButton();
+    } else {
+        isMoving = false;
+    }
+    
+    moveCounter++;
+}
+
+function rightChoice(){
+    window.alert('Parabéns, escolha certa!!! 😁❤😍')
+}
